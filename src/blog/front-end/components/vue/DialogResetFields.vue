1085b1ce7c33d89ab590f2f04612866f5bc64c9f
webB1an <webB1anyaoyao@gmail.com>
2023-11-02 09:23:12 +0800

    尝试修复按钮多显示问题

--- src/blog/front-end/components/vue/DialogResetFields.vue
+++ src/blog/front-end/components/vue/DialogResetFields.vue
@@ -1,22 +1,23 @@
 <template>
-  <div class="button-wrap">
+  <div class="app-container">
     <el-button type="primary" @click="handleAdd">新增</el-button>
     <el-button type="primary" @click="handleOpenDialog">修改</el-button>
+
+    <el-dialog title="弹窗" v-model="dialogFormVisible" width="500" @close="handleCloseDialog">
+      <el-form ref="form" :model="form" :rules="rules">
+        <el-form-item label="名称" prop="name">
+          <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item label="类型" prop="type">
+          <el-input v-model="form.type" />
+        </el-form-item>
+      </el-form>
+      <template v-slot:footer>
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleConfirmDialog">确 定</el-button>
+      </template>
+    </el-dialog>
   </div>
-  <el-dialog title="弹窗" v-model="dialogFormVisible" width="500" @close="handleCloseDialog">
-    <el-form ref="form" :model="form" :rules="rules">
-      <el-form-item label="名称" prop="name">
-        <el-input v-model="form.name" />
-      </el-form-item>
-      <el-form-item label="类型" prop="type">
-        <el-input v-model="form.type" />
-      </el-form-item>
-    </el-form>
-    <template v-slot:footer>
-      <el-button @click="dialogFormVisible = false">取 消</el-button>
-      <el-button type="primary" @click="handleConfirmDialog">确 定</el-button>
-    </template>
-  </el-dialog>
 </template>
 
 <script>
